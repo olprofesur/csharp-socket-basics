de8c88a3ec0ba881137f8fe839e9f6e909be8f92
olprofesur <diego.bernini@gmail.com>
2022-10-26 13:38:27 +0200

    esempi di base

---
+++ Socket03/SimpleSocketServer_multiclient_async_thread/ConsoleApp3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class SynchronousSocketListener
+{
+
+    // Incoming data from the client.  
+    public static string data = null;
+
+    public static void StartListening()
+    {
+
+
+        // Establish the local endpoint for the socket.  
+        // Dns.GetHostName returns the name of the   
+        // host running the application.  
+        IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5000);
+
+        // Create a TCP/IP socket.  
+        Socket listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        Console.WriteLine("Timeout : {0}",listener.ReceiveTimeout);
+
+        // Bind the socket to the local endpoint and   
+        // listen for incoming connections.  
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            // Start listening for connections.  
+            while (true)
+            {
+                
+                Console.WriteLine("Waiting for a connection...");
+                // Program is suspended while waiting for an incoming connection.  
+                Socket handler = listener.Accept();
+
+                ClientManager clientThread = new ClientManager(handler);
+                Thread t = new Thread(new ThreadStart(clientThread.doClient));
+                t.Start();
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static int Main(String[] args)
+    {
+        StartListening();
+        return 0;
+    }
+
+    
+}
+
+public class ClientManager
+{
+
+    Socket clientSocket;
+    byte[] bytes = new Byte[1024];
+    String data = "";
+
+    public ClientManager(Socket clientSocket)
+    {
+        this.clientSocket = clientSocket;
+    }
+
+    public void doClient()
+    {
+
+        while (data != "Quit$")
+        {
+            // An incoming connection needs to be processed.  
+            data = "";
+            while (data.IndexOf("$") == -1)
+            {
+                int bytesRec = clientSocket.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            }
+
+            // Show the data on the console.  
+            Console.WriteLine("Messaggio ricevuto : {0}", data);
+
+            // Echo the data back to the client.  
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+
+            clientSocket.Send(msg);
+        }
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+        data = "";
+
+    }
+}
